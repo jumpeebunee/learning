ca07e45ecfaf32a2dc2ae2723a3eff8bc0c17377
Egor Mukhaev <jumpeebunee@gmail.com>
2023-12-24 20:42:53 +0300

    Complete task

--- main.go
+++ main.go
@@ -4,15 +4,65 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
+var list gifts;
+
+
 func getInput() string {
 	reader := bufio.NewReader(os.Stdin);
-	name, _ := reader.ReadString('\n');
-	name = strings.TrimSpace(name);
+	input, _ := reader.ReadString('\n');
+	input = strings.TrimSpace(input);
 
-	return name;
+	return input;
+}
+
+func validateOption(option string) {
+	switch option {
+	case "s": 
+		saveCheck();
+	case "a": 
+		addNewItem();
+		chooseOption();
+	case "p" :
+		addPoints();
+		chooseOption();
+	}
+}
+
+func chooseOption() {
+	fmt.Print("Choose option (a - add item, s - save gift, p - add points): ");
+	option := getInput();
+	validateOption(option);
+}
+
+func addNewItem() {
+	fmt.Print("Item name: ");
+	name := getInput();
+	fmt.Print("Item price: ");
+	price := getInput();
+
+	val, _ := strconv.ParseFloat(price, 64);
+
+	list.addGift(name, val);
+	fmt.Printf("Item added: %v, price: %v rub \n", name, price);
+}
+
+func saveCheck() {
+	fmt.Println("Check has been saved");
+	fs := list.formattList();
+	fmt.Print(fs);
+}
+
+func addPoints() {
+	fmt.Print("Add points: ");
+	points := getInput();
+	val, _ := strconv.ParseFloat(points, 64);
+
+	list.updateScore(val);
+	fmt.Printf("Points has been updated to %v \n", points);
 }
 
 func createNewGift() gifts {
@@ -27,6 +77,6 @@ func createNewGift() gifts {
 }
 
 func main() {
-	giftList := createNewGift();
-	fmt.Print(giftList);
+	list = createNewGift();
+	chooseOption();
 }
